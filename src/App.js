cf3b532247dcaf9e0f923bacb812f03c9b3925c5
mohsinaliniyazi <mohsinaliniyazi786@gmail.com>
2024-06-13 10:13:00 +0530

    added video in react

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import logo from './sana.jpeg';
-import './App.css';
+import React from 'react';
+import sana from './images/sana.jpeg';
+import video from './video/new_year.mp4';
 
-function App() {
+import {
+  MDBContainer,
+  MDBCol,
+  MDBRow,
+} from 'mdb-react-ui-kit';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-          Jaan I Love You !!
-        
-      </header>
-    </div>
+    <MDBRow>
+      <MDBCol lg={4} md={12} className='mb-4 mb-lg-0'>
+      <div className="ratio ratio-16x9">
+        <iframe
+          src={video}
+          title="New Year Celebration"
+          allowfullscreen
+        ></iframe>
+      </div>
+      
+        <img
+          src={sana}
+          className='w-100 shadow-1-strong rounded mb-4'
+          alt='Boat on Calm Water'
+        />
+      
+      </MDBCol>
+    </MDBRow>
   );
 }
-
-export default App;
